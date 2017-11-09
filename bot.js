bb4a6064bff58b20a85403fe1108e948e289e2e2
jurrrgis <jurrrgis42@gmail.com>
2017-11-09 21:33:15 +0200

    Minor fixes

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const osu = require("node-osu");
 const moment = require("moment");
-const moment_tz = require("moment-timezone");
 const config = require("./config.json");
 const fs = require("fs");
 const move_decimal = require("move-decimal-point");
